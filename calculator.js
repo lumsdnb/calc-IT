1e2dc8859b860d6b753909fdfaa455a676051756
lums <jeremy@lums.io>
2020-11-02 13:53:06 +0100

    Make basic functions usable

--- calculator.js
+++ calculator.js
@@ -6,6 +6,12 @@ function subtract(a, b) {
   return a - b;
 }
 
-function multiply() {}
+function multiply(a, b) {
+  return a * b;
+}
 
-function divide() {}
+function divide(a, b) {
+  return a / b;
+}
+
+function operate(num1, num2) {}
