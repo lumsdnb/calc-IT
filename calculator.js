ecf3a16910dd0ed940a6ef048150d989e748807a
lums <jeremy@lums.io>
2020-11-02 18:52:26 +0100

    Add some broken code

--- calculator.js
+++ calculator.js
@@ -14,7 +14,10 @@ function divide(a, b) {
   return a / b;
 }
 
-function operate(num1, num2) {}
+function operate(num1, num2) {
+  if (num1 == null || num2 == null) return;
+  else console.log(`first number: ${num1}, second one is ${num2}`);
+}
 
 let display = document.querySelector('#display');
 
@@ -23,12 +26,18 @@ const buttons = document.querySelectorAll('button');
 let firstNumber = null;
 let secondNumber = null;
 
+function reset() {
+  display.textContent = 0;
+  firstNumber = null;
+  secondNumber = null;
+}
+
 //add event listeners to all buttons
 buttons.forEach((button) => {
   button.addEventListener('click', () => {
     console.log(button.id);
     if (button.id == 'clear') {
-      display.textContent = '0';
+      reset();
     }
     if (button.classList.contains('number')) {
       if (display.textContent.length > 9) return;
@@ -39,12 +48,16 @@ buttons.forEach((button) => {
       }
     }
     if (button.classList.contains('operator')) {
-      operate(firstNumber, secondNumber);
+      if (!firstNumber) {
+        firstNumber = display.textContent;
+      } else if (firstNumber && !secondNumber) {
+        secondNumber = display.textContent;
+        operate(firstNumber, secondNumber);
+      }
     }
   });
 });
 
-//if number is pressed, add it to display
 //if operator is pressed, save number from display to first var
 //if number after that is pressed, clear display and add it to display
 //if next button pressed is another operator or equals, compute result and show it in display
