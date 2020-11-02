deb242b65ea1a98736969e9751d6eae71ee33710
lums <jeremy@lums.io>
2020-11-02 15:22:49 +0100

    Add button IDs, get nodes in script

--- calculator.js
+++ calculator.js
@@ -15,3 +15,22 @@ function divide(a, b) {
 }
 
 function operate(num1, num2) {}
+
+const clearBtn = document.querySelector('#clear');
+const plusBtn = document.querySelector('#plus');
+const minusBtn = document.querySelector('#minus');
+const multiplyBtn = document.querySelector('#multiply');
+const divideBtn = document.querySelector('#divide');
+const equalsBtn = document.querySelector('#equals');
+
+const zeroBtn = document.querySelector('#zero');
+const pointBtn = document.querySelector('#point');
+const oneBtn = document.querySelector('#one');
+const twoBtn = document.querySelector('#two');
+const threeBtn = document.querySelector('#three');
+const fourBtn = document.querySelector('#four');
+const fiveBtn = document.querySelector('#five');
+const sixBtn = document.querySelector('#six');
+const sevenBtn = document.querySelector('#seven');
+const eightBtn = document.querySelector('#eight');
+const nineBtn = document.querySelector('#nine');
