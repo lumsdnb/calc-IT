56986e5a9d6dd815636a993428fe42ee8faadd10
lums <jeremy@lums.io>
2020-11-02 16:56:01 +0100

    Simplified button vars into single one

--- calculator.js
+++ calculator.js
@@ -16,21 +16,9 @@ function divide(a, b) {
 
 function operate(num1, num2) {}
 
-const clearBtn = document.querySelector('#clear');
-const plusBtn = document.querySelector('#plus');
-const minusBtn = document.querySelector('#minus');
-const multiplyBtn = document.querySelector('#multiply');
-const divideBtn = document.querySelector('#divide');
-const equalsBtn = document.querySelector('#equals');
+let display = document.querySelector('#display');
 
-const zeroBtn = document.querySelector('#zero');
-const pointBtn = document.querySelector('#point');
-const oneBtn = document.querySelector('#one');
-const twoBtn = document.querySelector('#two');
-const threeBtn = document.querySelector('#three');
-const fourBtn = document.querySelector('#four');
-const fiveBtn = document.querySelector('#five');
-const sixBtn = document.querySelector('#six');
-const sevenBtn = document.querySelector('#seven');
-const eightBtn = document.querySelector('#eight');
-const nineBtn = document.querySelector('#nine');
+const buttons = document.querySelectorAll('button');
+
+let firstNumber = null;
+let secondNumber = null;
